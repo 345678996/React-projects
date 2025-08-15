6b410c2d220304d9e55fd71d1e9ee0936d89ff46
Kanav Rohatgi <kanavr@gmail.com>
2025-08-15 19:40:38 +0530

    color picker without css

--- color-picker/src/App.jsx
+++ color-picker/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [backgroundColor, setBackgroundColor] = useState("#ffffff");
+  const colors = ["#ff0000", "#FAEBD7", "#00FFFF", "#D2691E"];
+
+  const handleColorChange = (color) => {
+    setBackgroundColor(color);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App" style={{ backgroundColor: backgroundColor }}>
+      <h1>Color Picker</h1>
+      <div className="color-palette"></div>
+      {colors.map((color, index) => (
+        <div
+          key={index}
+          className="color-box"
+          style={{ backgroundColor: color }}
+          onClick={() => handleColorChange(color)}
+        ></div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
